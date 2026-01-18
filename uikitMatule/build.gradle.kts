d4030d73fa93f0e46b4595c3c46d1dcbfc122cff
Kirill-wq333 <154874212+Kirill-wq333@users.noreply.github.com>
2026-01-18 22:24:18 +0400

    sprint_1

--- uikitMatule/build.gradle.kts
+++ uikitMatule/build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.example.matule20"
+    namespace = "com.example.matulelibrary"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.matule20"
         minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,9 +32,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
@@ -46,16 +39,14 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.material)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation(project(":uikitMatule"))
 }
---
+++ uikitMatule/src/main/java/com/example/matulelibrary/spacers/MatuleSpacers.kt
@@ -0,0 +1,17 @@
+package com.example.matulelibrary.spacers
+
+import androidx.compose.ui.unit.dp
+
+object MatuleSpacers {
+    val extraSmall = 4.dp
+    val small = 8.dp
+    val medium = 12.dp
+    val mediumLarge = 16.dp
+    val large = 20.dp
+    val extraLarge = 24.dp
+    val xxl = 32.dp
+    val xxl2 = 40.dp
+    val xxl3 = 48.dp
+    val xxl4 = 56.dp
+    val xxl5 = 64.dp
+}
